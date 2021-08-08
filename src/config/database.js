d60810ec9fbb4a0790ead98b3af4cba8ab5e8f72
Matheus Barth <matheusbarthf@gmail.com>
2021-08-08 11:35:27 -0300

    Configuração inicial

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'root',
+  password: 'root',
+  database: 'teste_backend',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
